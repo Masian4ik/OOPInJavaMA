17d40b4e50be99d886b2dc93701e4ecf32b40c6e
maksimnovikov <masiandr23@gmail.com>
2019-07-01 23:56:56 +0300

    Add Runner class

---
+++ src/com/mainacad/ApplicationRunner.java
@@ -0,0 +1,47 @@
+package com.mainacad;
+
+import com.mainacad.model.AbstractShape;
+import com.mainacad.model.Circle;
+import com.mainacad.model.Square;
+import com.mainacad.model.Triangle;
+
+
+public class ApplicationRunner {
+
+
+    public static void main(String[] args) {
+
+        Circle circle1 = new Circle();
+
+        circle1.setRadius(10);
+
+        System.out.println(" Circle with radius " + circle1.getRadius() + " has area " + circle1.getArea());
+
+        Square square = new Square();
+
+        square.setSide(15);
+
+        System.out.println(" Square with side " + square.getSide() + " has area " + square.getArea());
+
+        Triangle triangle = new Triangle( 12, 15 );
+        AbstractShape triangleNew = new Triangle();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
