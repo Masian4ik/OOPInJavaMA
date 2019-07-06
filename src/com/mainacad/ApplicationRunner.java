0a2e94a81dc73dd48075d195c14f6e55bbd75eba
maksimnovikov <masiandr23@gmail.com>
2019-07-06 18:21:25 +0300

    Add if statements

--- src/com/mainacad/ApplicationRunner.java
+++ src/com/mainacad/ApplicationRunner.java
@@ -1,9 +1,6 @@
 package com.mainacad;
 
-import com.mainacad.model.AbstractShape;
-import com.mainacad.model.Circle;
-import com.mainacad.model.Square;
-import com.mainacad.model.Triangle;
+import com.mainacad.model.*;
 
 
 public class ApplicationRunner {
@@ -11,24 +8,50 @@ public class ApplicationRunner {
 
     public static void main(String[] args) {
 
-        Circle circle1 = new Circle();
+        Circle circle = new Circle(10);
 
-        circle1.setRadius(10);
-
-        System.out.println(" Circle with radius " + circle1.getRadius() + " has area " + circle1.getArea());
-
-        Square square = new Square();
-
-        square.setSide(15);
-
-        System.out.println(" Square with side " + square.getSide() + " has area " + square.getArea());
+        Square square = new Square(15);
 
         Triangle triangle = new Triangle(12, 15);
-        AbstractShape triangleNew = new Triangle();
 
+        Quadrilateral quadrilateral = new Quadrilateral(12, 20);
+
+        Shape maxShape;
+
+        if (circle.getArea() > square.getArea()) {
+            maxShape = circle;
+        }
+        else if (circle.getArea() < square.getArea()){
+            maxShape = square;
+        }
+
+        else {
+            maxShape = square;
+        }
+
+        if (maxShape.getArea() > triangle.getArea()) {
+            maxShape = triangle;
+
+        }
+        System.out.println("Max shape has area " + maxShape.getArea());
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
